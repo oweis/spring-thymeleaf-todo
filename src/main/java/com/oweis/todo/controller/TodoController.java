68fc57bc4080facae038368c7c8b2eeadfc3b618
Yahya Oweis <oweisyahya@gmail.com>
2020-12-25 23:20:26 +0100

    change entrypoints values

--- src/main/java/com/oweis/todo/controller/TodoController.java
+++ src/main/java/com/oweis/todo/controller/TodoController.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import com.oweis.todo.repository.TodoRepository;
 
 @Controller
@@ -16,20 +14,20 @@ public class TodoController {
     @Autowired
     TodoRepository todoRepository;
 
-    @GetMapping("/")
+    @GetMapping(value={"/todos","/"})
     public String index(Todo todo, BindingResult result,Model model){
         model.addAttribute("todoList", todoRepository.findByIsDoneFalse());
         model.addAttribute("doneList", todoRepository.findByIsDoneTrue());
         return "index";
     }
 
-    @PostMapping("/addtodo")
+    @PostMapping("/todos")
     public String addTodo(Todo todo, BindingResult result, Model model) {
         todoRepository.save(todo);
         return "redirect:/";
     }
 
-    @GetMapping("/todo/{id}/markdone")
+    @GetMapping("/todos/{id}/markdone")
     public String markdoneTodo(@PathVariable("id") long id, Model model) {
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid todo Id:" + id));
@@ -38,7 +36,7 @@ public class TodoController {
         return "redirect:/";
     }
 
-    @GetMapping("/todo/{id}/marktodo")
+    @GetMapping("/todos/{id}/marktodo")
     public String marktodoTodo(@PathVariable("id") long id, Model model) {
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid todo Id:" + id));
@@ -47,7 +45,7 @@ public class TodoController {
         return "redirect:/";
     }
 
-    @GetMapping("/delete/{id}")
+    @GetMapping("/todos/{id}/delete")
     public String deleteTodo(@PathVariable("id") long id, Model model) {
         Todo todo = todoRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid todo Id:" + id));
